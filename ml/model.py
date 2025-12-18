6bc600e23defeb303142e33e90b67f7325edb641
Lucy <lucbhickey@gmail.com>
2025-12-18 07:23:07 -0500

    Set classifier

--- ml/model.py
+++ ml/model.py
@@ -3,6 +3,7 @@ from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 import pandas as pd
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 
 # Optional: implement hyperparameter tuning.
@@ -21,6 +22,7 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
+    clf_model = RandomForestClassifier(n_estimators=100, random_state=69)
     
     pass
 
